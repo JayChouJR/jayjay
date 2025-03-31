296d7a2928a5ef9184eceb0a294ffc65294b7f12
kevin <kevinmedeiroliver21@gmail.com>
2025-03-30 22:35:25 -0300

    CHG : servindo arquivos estaticos

--- app/script/serve.js
+++ app/script/serve.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const root = require('../router/router.js'); // Importa as rotas
+
 const app = express();
 
+// Usa as rotas do arquivo separado
+app.use('/', root);
+app.use(express.static(path.join(__dirname, '../')));
+
+// Servir arquivos estáticos (ex: home.html)
 app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, '../view/home.html'));
+  res.sendFile(path.join(__dirname, '../view/detalhes.html'));
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+  console.log('Servidor rodando em http://localhost:3000');
 });
